377cd759f5e82c00f2aeb453cd22ae79fb8d5713
Jonathan Herrera <Jonathan.a.herrera0@gmail.com>
2024-08-11 17:44:04 -0500

    Create basic UI

--- chatsupport/app/api/chat/route.js
+++ chatsupport/app/api/chat/route.js
@@ -4,7 +4,7 @@ import {NextResponse} from 'next/server'
 
 export async function POST(req){
     const promptResponse = await req.json()
-    const prompt = promptResponse.prompt;
+    const prompt = promptResponse.content;
 
     const { GoogleGenerativeAI } = require("@google/generative-ai");
     const systemPrompt = 'You are fAIshonBot, the friendly and knowledgeable customer service assistant for fAIshon, a cutting-edge fashion company. Your role is to assist customers with inquiries related to fAIshon’s products, orders, returns, and fashion advice. Provide helpful, accurate, and personalized responses while maintaining a warm and engaging tone. Always prioritize customer satisfaction and make sure to understand the context of each query before responding. Ensure all responses reflect the brand\'s values of style, innovation, and customer care.'
